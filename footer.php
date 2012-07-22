33bd69f69c126ffe653f7d928723359ceab7709c
Giovanni Keppelen <keppelen.as@gmail.com>
2012-07-22 17:47:22 -0300

    wordpress theme created, listing last news, confs, casts and labs #13

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer>
+
+    <div class="wrapper">
+
+      <img src="<?php bloginfo('template_url'); ?>/img/logo-pb.png" alt="BrazilJS Logo">
+
+      <p class="about">BrazilJS Foundation is a non-profit foundation with the mission to Moving and Uniting the web community in Brazil.</p>
+
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Team</a></li>
+        <li><a href="#">Manifesto</a></li>
+        <li><a href="#">Sponsors</a></li>
+        <li><a href="#">Get involved</a></li>
+        <li><a href="#">Contact us</a></li>
+      </ul>
+
+      <ul class="social">
+        <li><a href="#">Twitter @BrazilJS</a></li>
+        <li><a href="#">Google Plus</a></li>
+        <li><a href="#">Facebook</a></li>
+      </ul>
+
+      <p class="license">© 2012. Content available under a <a href="#">Creative Commons</a> license.</p>
+
+    </div>
+
+  </footer>
+
+  <!-- Tweet Button -->
+  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+</body>
+</html>

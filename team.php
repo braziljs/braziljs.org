c6a21e2d90dacfab27bf003ede607f0c60288a9c
zenorocha <zno.rocha@gmail.com>
2012-07-24 01:34:39 -0300

    avatar links added

--- team.php
+++ team.php
@@ -13,37 +13,37 @@
         <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae orci eget dui semper consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae orci eget dui semper.</p>
       </li>
       <li>
-        <a href="#"><img src="img/avatar/davidson.jpg" alt="Davidson Fellipe"></a>
+        <a href="http://twitter.com/davidsonFellipe" target="_blank"><img src="img/avatar/davidson.jpg" alt="Davidson Fellipe"></a>
         <p class="name"><a href="http://twitter.com/davidsonFellipe" target="_blank">Davidson Fellipe</a></p>
         <p class="position">Regional Conference Lead</p>
         <p class="description">Founder and moderator of RioJS and PernambucoJS (JS user groups), graduated in Computer Engineering at UPE. Currently is Front-end engineer at Globo.com and Master degree Candidate at PUC-Rio.</p>
       </li>
       <li>
-        <a href="#"><img src="img/avatar/felipe.jpg" alt=""></a>
+        <a href="http://twitter.com/felipenmoura" target="_blank"><img src="img/avatar/felipe.jpg" alt=""></a>
         <p class="name"><a href="http://twitter.com/felipenmoura" target="_blank">Felipe N. Moura</a></p>
         <p class="position">National Conference Lead</p>
         <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae orci eget dui semper consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae orci eget dui semper.</p>
       </li>
       <li>
-        <a href="#"><img src="img/avatar/giovanni.jpg" alt=""></a>
+        <a href="http://twitter.com/keppelen" target="_blank"><img src="img/avatar/giovanni.jpg" alt=""></a>
         <p class="name"><a href="http://twitter.com/keppelen" target="_blank">Giovanni Keppelen</a></p>
         <p class="position">Regional Conference Lead</p>
         <p class="description">Idealizador e organizador do @frontInBH e moderador do Rio.js, graduando em Produção Multimidia pela UNI-BH, Front-end engineer com foco em Performance no Peixe Urbano e Tech leader.</p>
       </li>
       <li>
-        <a href="#"><img src="img/avatar/jaydson.jpg" alt=""></a>
+        <a href="http://twitter.com/jaydson" target="_blank"><img src="img/avatar/jaydson.jpg" alt=""></a>
         <p class="name"><a href="http://twitter.com/jaydson" target="_blank">Jaydson Gomes</a></p>
         <p class="position">National Conference Lead</p>
         <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae orci eget dui semper consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae orci eget dui semper.</p>
       </li>
       <li>
-        <a href="#"><img src="img/avatar/leo.jpg" alt=""></a>
+        <a href="http://twitter.com/leobalter" target="_blank"><img src="img/avatar/leo.jpg" alt=""></a>
         <p class="name"><a href="http://twitter.com/leobalter" target="_blank">Leo Balter</a></p>
         <p class="position">Community Blog Lead</p>
         <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae orci eget dui semper consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae orci eget dui semper.</p>
       </li>
       <li>
-        <a href="http://zenorocha.com/"><img src="img/avatar/zeno.jpg" alt=""></a>
+        <a href="http://zenorocha.com/" target="_blank"><img src="img/avatar/zeno.jpg" alt=""></a>
         <p class="name"><a href="http://zenorocha.com/" target="_blank">Zeno Rocha</a></p>
         <p class="position">Community Relations Lead</p>
         <p class="description">One of the creators of <a href="http://jqueryboilerplate.com">jQuery Boilerplate</a>, <a href="http://www.chromeexperiments.com/detail/wormz/">Wormz</a> and <a href="//diveintohtml5.com.br/">Dive into HTML5</a> translation lead. With only 21 years-old, he already worked as a software developer at Petrobras and Globoesporte.com. Now he is a Front-end Engineer at <a href="http://liferay.com/">Liferay</a>.</p>

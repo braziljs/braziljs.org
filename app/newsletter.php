f8e06ef51f2b3e3b31be00eaed7626cf6c4d4d98
Davidson Fellipe <email@fellipe.com>
2012-07-24 00:27:35 -0300

    config files for database local and production - issue #38

--- app/newsletter.php
+++ app/newsletter.php
@@ -1,12 +1,13 @@
 <?php
 
+	require_once('../config/config.php');
+
 	$MSG_ERROR = "error";
 	$MSG_SUCCESS = "<strong>Great</strong>, now you're in! <a href='#'' class='more-one'>Do you want to subscribe another email?</a>";
 
-
 	if( isset( $_POST['email'] ) && strlen($_POST['email']) > 1 ){
 
-		$con = mysql_connect("localhost","root","root");
+		$con = mysql_connect(DATABASE_SERVER, DATABASE_USER, DATABASE_PASSWORD);
 
 		$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 
@@ -16,7 +17,7 @@
 
 		if($email) {
 
-			mysql_select_db("braziljs", $con);
+			mysql_select_db(DATABASE_NAME, $con);
 
 			$query = "INSERT INTO newsletter (email, date) VALUES ('" . $email ."', NOW())";
 

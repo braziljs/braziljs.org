67c45fdc577d45964499e3b120131a870fc45603
zenorocha <zno.rocha@gmail.com>
2012-07-24 01:04:22 -0300

    adjusting sucess message

--- app/newsletter.php
+++ app/newsletter.php
@@ -3,7 +3,7 @@
 	require_once('../config/config.php');
 
 	$MSG_ERROR = "error";
-	$MSG_SUCCESS = "<strong>Great</strong>, now you're in! <a href='#'' class='more-one'>Do you want to subscribe another email?</a>";
+	$MSG_SUCCESS = "<strong>Great</strong>, now you're in! Do you want to subscribe <a href='#' class='more-one'>another email?</a>";
 
 	if( isset( $_POST['email'] ) && strlen($_POST['email']) > 1 ){
 

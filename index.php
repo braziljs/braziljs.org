b80c1f4d58a28f5cea81f7ad0f2d041e9428e399
zenorocha <zno.rocha@gmail.com>
2012-07-25 19:39:18 -0300

    braziljs google group url changed

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
     <img src="img/img-getinvolved.png" alt="Brazilian Heart">
     <h3>Join us</h3>
-    <p>We are starting a movement to unify the JavaScript community in Brazil, both front-end and back-end. Stay tuned for the cool things we are preparing. <a href="https://groups.google.com/forum/?fromgroups#!forum/braziljs-group">Join the discussion group</a>.</p>
+    <p>We are starting a movement to unify the JavaScript community in Brazil, both front-end and back-end. Stay tuned for the cool things we are preparing. <a href="https://groups.google.com/forum/?fromgroups#!forum/braziljs-foundation">Join the discussion group</a>.</p>
 
     <form id="get-involved-form">
       <input id="get-involved-form-email" name="email" type="text" placeholder="Enter your email here">

36e4ecb3ea3b5d459518c1bf21d414bb9d8eec15
Zeno Rocha <zno.rocha@gmail.com>
2012-11-01 00:37:13 -0200

    creating projects template and grabbing custom url attribute

---
+++ page-projects.php
@@ -0,0 +1,42 @@
+<?php /* Template Name: Projects */
+
+  get_header(); ?>
+
+  <section class="content">
+
+    <div class="wrapper">
+
+      <h2>Projetos</h2>
+
+        <?php query_posts('cat=7&showposts=-1'); ?>
+
+          <?php if ( have_posts() ) : while( have_posts() ) : the_post() ?>
+
+          <div>
+
+            <?php
+
+              $onde = get_post_custom_values('onde');
+
+              // Título
+              if ($onde[0] != '') {
+                echo '<a href="' . $onde[0] . '" target="_blank">'; the_title(); echo '</a>';
+              } else {
+                the_title();
+              }
+
+            ?>
+
+        <br /><br /><br /><br />
+
+        </div>
+
+        <?php endwhile; endif; wp_reset_query(); ?>
+
+      </div>
+
+    </div>
+
+  </section>
+
+  <?php get_footer(); ?>

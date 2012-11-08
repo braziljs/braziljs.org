d47b91bbb3b70e9c2376f4db1ab40c6f2f551b03
Zeno Rocha <zno.rocha@gmail.com>
2012-11-08 14:30:44 -0200

    Remove 'foundation' text on footer #62

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 
       <img src="<?php bloginfo('template_url'); ?>/img/logo-pb.png" alt="BrazilJS Logo" width="150" height="51">
 
-      <p class="about">BrazilJS é uma fundação sem fins lucrativos com a missão de mover e unir a comunidade de JavaScript no Brasil.</p>
+      <p class="about">BrazilJS é uma fundação sem fins lucrativos com a missão de mover e unir a comunidade em torno do JavaScript no Brasil.</p>
 
       <ul class="nav">
         <li><a href="<?php bloginfo('home'); ?>/team" title="Time">Time</a></li>

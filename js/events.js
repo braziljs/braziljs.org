751b342dddd41f74e42dbc2b779087e89c6f8b2e
keppelen <keppelen.as@gmail.com>
2012-11-11 20:15:48 -0200

    usando template para mostrar os eventos

--- js/events.js
+++ js/events.js
@@ -1,41 +1,28 @@
-$(function() {
+$(document).ready(function(){
 
-  $.getJSON("http://braziljs.org/services/events/2012.json", function (result) {
-
-    EVENT = '<div class="event">' +
-              '<div class="event-media">' +
-                '<a href="#{website}" title="#{name}">' +
-                  '<img src="#{thumbnail}" alt="#{name}" />' +
-                  '<span class="event-date">#{date}</span>' +
-                  '<span class="event-price">#{price}</span>' +
-                '</a>' +
-              '</div>' +
-              '<h1 class="event-name"><a href="#{website}" title="#{name}">#{name}</a></h1>' +
-              '<span class="event-city">#{location}</span>' +
-            '</div>';
-
-    if (result.events && result.events.length > 0) {
-
-      var events = "", item = "";
-
-      for ( var i = 0; i < result.events.length ; i++ ) {
-
-        events += EVENT.replace("#{website}", result.events[i].website)
-                       .replace("#{name}", result.events[i].name)
-                       .replace("#{thumbnail}", result.events[i].thumbnail)
-                       .replace("#{name}", result.events[i].name)
-                       .replace("#{date}", result.events[i].date)
-                       .replace("#{price}", result.events[i].price)
-                       .replace("#{website}", result.events[i].website)
-                       .replace("#{name}", result.events[i].name)
-                       .replace("#{name}", result.events[i].name)
-                       .replace("#{location}", result.events[i].location);
-      }
-
-      $('.content .wrapper').append(events);
-
-    }
-
+  $.getJSON('http://braziljs.org/services/events/2012.json', function(response) {
+    MontaEvents(response);
   });
 
-});
+  var data = new Date(),
+      dia = data.getDate(),
+      mes = data.getMonth(),
+      ano = data.getFullYear(),
+      dateAtual = ano + "-" + mes + "-" + dia;
+
+  function MontaEvents (data) {
+    var template = Events.getTemplate($("#list-events")); 
+    var html = template(data);
+    $('#events').html(html);
+  } 
+
+})
+
+var Events = {
+
+  getTemplate: function(template){
+    var source   = template.html();
+    return Handlebars.compile(source);
+  },
+
+}
--- page-events.php
+++ page-events.php
@@ -3,10 +3,30 @@
   get_header(); ?>
 
   <section class="content">
-    <div class="wrapper"></div>
+    <div class="wrapper" id="events"></div>
   </section>
 
+  <script type="text/x-handlebars-template" id="list-events">
+  	<h1>Proximos eventos</h1>
+  	{{#each events}}
+  	
+		<div class="event">
+	       <div class="event-media">
+	         <a href="{{website}}" title="{{name}}">
+	           <img src="{{thumbnail}}" alt="{{name}}"/>
+	           <span class="event-date">{{date}}</span>
+	           <span class="event-price">{{price}}</span>
+	         </a>
+	       </div>
+	       <h1 class="event-name"><a href="{{website}}" title="{{name}}">{{name}}</a></h1>
+	       <span class="event-city">{{location}}</span>
+	    </div>
+
+    {{/each}}
+  </script>
+
   <script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/handlebars.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/events.js"></script>
 
   <?php get_footer(); ?>

973c582bb41a3504653a68723afa946de1872abd
keppelen <keppelen.as@gmail.com>
2012-11-11 21:56:59 -0200

    remove handlebars, divisoria proximos e anteriores

--- js/events.js
+++ js/events.js
@@ -1,28 +1,37 @@
 $(document).ready(function(){
 
-  $.getJSON('http://braziljs.org/services/events/2012.json', function(response) {
-    MontaEvents(response);
-  });
+  $.getJSON("http://braziljs.org/services/events/2012.json", function (response) {
+  //$.getJSON('/themeBrazilJS/wp-content/themes/foundation/js/2012.json', function(response) {
 
   var data = new Date(),
       dia = data.getDate(),
-      mes = data.getMonth(),
+      mes = data.getMonth() + 1,
       ano = data.getFullYear(),
       dateAtual = ano + "-" + mes + "-" + dia;
 
-  function MontaEvents (data) {
-    var template = Events.getTemplate($("#list-events")); 
-    var html = template(data);
-    $('#events').html(html);
-  } 
+    for ( var i = 0; i < response.events.length ; i++ ) {
+
+      EVENT = '<div class="event">' +
+           '<div class="event-media">' +  
+              '<a href="'+ response.events[i].website +'" title="'+ response.events[i].name +'">' + 
+                '<img src="'+ response.events[i].thumbnail +'" alt="'+ response.events[i].name +'" />' +
+                '<span class="event-date">'+ response.events[i].date +'</span>' +
+                '<span class="event-price">'+ response.events[i].price +'</span>' +
+              '</a>' +
+            '</div>' +
+            '<h1 class="event-name"><a href="'+ response.events[i].website +'" title="'+ response.events[i].name +'">'+ response.events[i].name +'</a></h1>' +
+            '<span class="event-city">'+ response.events[i].location +'</span>' +
+          '</div>';
+
+      if ( response.events[i].date > dateAtual ) {
+        $("#event-new").append(EVENT);
+      }
+      else {
+        $("#events-older").append(EVENT);
+      }
+      
+    }
+
+  });
 
 })
-
-var Events = {
-
-  getTemplate: function(template){
-    var source   = template.html();
-    return Handlebars.compile(source);
-  },
-
-}
--- page-events.php
+++ page-events.php
@@ -3,29 +3,16 @@
   get_header(); ?>
 
   <section class="content">
-    <div class="wrapper" id="events"></div>
+    <div class="wrapper" id="events">
+      <h3>Próximos eventos</h3>
+      <div id="event-new"></div>
+
+      <h3>Eventos Anteriores</h3>
+      <div id="events-older"></div>
+    </div>
   </section>
 
-  <script type="text/x-handlebars-template" id="list-events">
-  	{{#each events}}
-
-		<div class="event">
-	       <div class="event-media">
-	         <a href="{{website}}" title="{{name}}">
-	           <img src="{{thumbnail}}" alt="{{name}}"/>
-	           <span class="event-date">{{date}}</span>
-	           <span class="event-price">{{price}}</span>
-	         </a>
-	       </div>
-	       <h1 class="event-name"><a href="{{website}}" title="{{name}}">{{name}}</a></h1>
-	       <span class="event-city">{{location}}</span>
-	    </div>
-
-    {{/each}}
-  </script>
-
   <script src="http://code.jquery.com/jquery-1.8.0.min.js"></script>
-  <script src="<?php bloginfo('template_url'); ?>/js/handlebars.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/events.js"></script>
 
   <?php get_footer(); ?>

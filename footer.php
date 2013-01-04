0ece5b1d7b03e65d4bf2eb921aae0e81710e475b
yeltco <hi@yelt.co>
2013-01-03 22:39:10 -0800

    wp_footer()
    Carrega os scripts padrões do WordPress no footer

--- footer.php
+++ footer.php
@@ -29,6 +29,9 @@
   (function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1&appId=372862979453673";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
   !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
   </script>
-
+  
+  <!-- Carrega os scripts padrões do WordPress no footer -->
+  <?php wp_footer(); ?>
+  
 </body>
 </html>

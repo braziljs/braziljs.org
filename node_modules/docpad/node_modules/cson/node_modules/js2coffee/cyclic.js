1d41bc16c4c4eacae9c5e3d54c800296da4ddc6b
Zeno Rocha <zeno.rocha@liferay.com>
2013-11-29 19:17:31 -0800

    Ignore node_modules and remove unnecessary .eco extension on projects

--- node_modules/docpad/node_modules/cson/node_modules/js2coffee/cyclic.js
+++
@@ -1,7 +0,0 @@
-if ( require('fs').existsSync('.git') ) {
-	require('child_process').spawn(
-		'npm',
-		['install', '--force', require('./package.json').name],
-		{env:process.env, cwd:process.cwd(), stdio:'inherit'}
-	);
-}

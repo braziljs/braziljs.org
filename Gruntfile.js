2e74b5812834c3eae7053391db634ab4a7b7574e
Zeno Rocha <zeno.rocha@liferay.com>
2014-02-20 13:26:45 -0300

    Adiciona tarefa no Grunt para validar HTML gerado #123

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    validation: {
+      options: {
+        reportpath: false,
+        reset: true,
+        stoponerror: false
+      },
+      files: {
+        src: ['out/**/*.html']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-html-validation');
+
+};
